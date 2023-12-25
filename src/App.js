ec773d219504dd32fa10f58ddb997a60e0709b47
Roudabeh Adnani <rodabeh.adnani@gmail.com>
2023-12-26 00:54:42 +0100

    initiate app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
-  
+  const [location, setLocation] = useState('');
+
+  const onChangeHandler = (e) => { 
+    setLocation(e.target.value)
+
+  }
+
+  const fetchWeather = ()=>{
+    console.log("Loading data...")
+    console.log(location)
+  }
+
   return (
-    <div>
-      <button>+</button>
-      <span>Count</span>
-      <button>-</button>
+    <div className='app'>
+      <h1>Classy Weather</h1>
+      <input type='text' placeholder='Search From Location' value={location} onChange={onChangeHandler}/>
+      <button onClick={fetchWeather}>Get Weather</button>
+
     </div>
   )
 }
